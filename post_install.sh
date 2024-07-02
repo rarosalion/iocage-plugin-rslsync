4bbf9f025d96abaada787e557e1971b5836f60c7
rarosalion <6749958+rarosalion@users.noreply.github.com>
2024-07-02 21:52:47 +1000

    fix: fix rm

--- post_install.sh
+++ post_install.sh
@@ -11,7 +11,7 @@ chown rslsync:rslsync /var/db/rslsync
 ## install bin
 fetch -o /tmp https://download-cdn.resilio.com/stable/freebsd/x64/0/resilio-sync_freebsd_x64.tar.gz
 tar xvf /tmp/resilio-sync_freebsd_x64.tar.gz -C /usr/local/bin/
-rm /usr/local/bin/LICENSE.TXT /tmp/resilio-sync_freebsd_x64.tar.gz
+rm -f /usr/local/bin/LICENSE.TXT /tmp/resilio-sync_freebsd_x64.tar.gz
 
 # enable the service
 sysrc rslsync_enable="YES"
